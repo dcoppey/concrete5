ce03ce116a241e0b06d58b2318e14ef104bc9442
Damien Coppey <damien.coppey@orange.Fr>
2013-01-23 10:16:05 +0100

    installation de concrete5
    Signed-off-by: Damien Coppey <damien.coppey@orange.Fr>

---
+++ config/site.php
@@ -0,0 +1,6 @@
+<?php 
+define('DB_SERVER', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_DATABASE', 'concrete5');
+define('PASSWORD_SALT', 'sVy9yIZljbsyAxoVBlw06z5BYRwoghEVRIXpJhfREeW4HzKzq7T8U0ZchniZq9lg');
